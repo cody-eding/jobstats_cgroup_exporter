b4af4678f9f7640980f086b7d15fa1a96b72d9ea
Cody Eding <edingco@gvsu.edu>
2026-02-06 09:49:51 -0500

    keep more dirs

--- collector/cgroupv2.go
+++ collector/cgroupv2.go
@@ -87,7 +87,7 @@ func getNamev2(pidPath string, path string, logger log.Logger) string {
 	dirs := strings.Split(pidPath, "/")
 	var name string
 	// if strings.Contains(path, "slurm") {
-	keepDirs := dirs[0:4]
+	keepDirs := dirs[0:6]
 	name = strings.Join(keepDirs, "/")
 	// } else {
 	// 	keepDirs := dirs[0:3]
