9a6ae393e3ef03df30a0c1d4044e5131508a0c06
Cody Eding <edingco@gvsu.edu>
2026-02-06 09:15:43 -0500

    add debug

--- collector/cgroupv2.go
+++ collector/cgroupv2.go
@@ -38,6 +38,7 @@ var (
 func getInfov2(name string, pids []int, metric *CgroupMetric, logger log.Logger) {
 	slurmPattern := regexp.MustCompile("/job_([0-9]+)(/step_([^/]+)(/user/task_([0-9]+|special))?)?$")
 	slurmMatch := slurmPattern.FindStringSubmatch(name)
+	level.Debug(logger).Log("msg", "Got for match", "name", name, "len(slurmMatch)", len(slurmMatch), "slurmMatch", fmt.Sprintf("%v", slurmMatch))
 	if len(slurmMatch) == 6 {
 		metric.job = true
 		metric.jobid = slurmMatch[1]
