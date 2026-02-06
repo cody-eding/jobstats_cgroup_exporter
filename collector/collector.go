ae3df977003be4e086e13c4882a91407c39a79fa
Cody Eding <edingco@gvsu.edu>
2026-02-06 08:49:31 -0500

    uid

--- collector/collector.go
+++ collector/collector.go
@@ -161,6 +161,7 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 		if m.err {
 			ch <- prometheus.MustNewConstMetric(e.collectError, prometheus.GaugeValue, 1, m.name)
 		}
+		ch <- prometheus.MustNewConstMetric(e.uid, prometheus.GaugeValue, m.uid, m.jobid)
 		ch <- prometheus.MustNewConstMetric(e.cpuUser, prometheus.GaugeValue, m.cpuUser, m.name, m.jobid)
 		ch <- prometheus.MustNewConstMetric(e.cpuSystem, prometheus.GaugeValue, m.cpuSystem, m.name, m.jobid)
 		ch <- prometheus.MustNewConstMetric(e.cpuTotal, prometheus.GaugeValue, m.cpuTotal, m.name, m.jobid)
